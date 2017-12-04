04adead0fc77eb4d2eb72fcab9313c3643af9ce1
Victor Castillo <mini.guero@hotmail.com>
2017-12-03 18:54:11 -0600

    Roles models ready

--- roles/models.py
+++ roles/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 # Create your models here.
+
+
+class Roles(TimeStampedModel):
+	ORGANIZATION_ADMIN = 'organization_admin'
+	SELLER = 'seller'
+
+	active = models.BooleanField(default=True)
+	key = models.CharField(max_length=20,unique=True)
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
