603c68c33d81227149d1ea5517f75f31736a1a3b
Victor Castillo <mini.guero@hotmail.com>
2017-12-12 23:25:03 -0600

    changes to gunicorn

--- buildings/views.py
+++ buildings/views.py
@@ -3,10 +3,21 @@ from rest_framework.response import Response
 from buildings.models import SimpleApartment
 from buildings.serializers import SimpleApartmentSerializer
 from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 # Create your views here.
 
-@api_view(['GET'])
+# @api_view(['GET'])
+# def get_simple_apartment_info(request, apartment_id):
+# 	apartment = SimpleApartment.objects.get(id=apartment_id)
+# 	return Response(SimpleApartmentSerializer(apartment).data)
+
+
 def get_simple_apartment_info(request, apartment_id):
 	apartment = SimpleApartment.objects.get(id=apartment_id)
-	return Response(SimpleApartmentSerializer(apartment).data)
+	return JsonResponse({
+		'price': apartment.price,
+		'id': apartment.id
+	})
+
+
