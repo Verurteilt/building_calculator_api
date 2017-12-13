be5ae8b9b9e9753916639722f8180ef8411764b8
Victor Castillo <mini.guero@hotmail.com>
2017-12-12 22:14:16 -0600

    changes to gunicorn

--- building_calculator_api/settings.py
+++ building_calculator_api/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 
 # Django rest framework configuration
--- building_calculator_api/urls.py
+++ building_calculator_api/urls.py
@@ -18,9 +18,12 @@ from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls import url
 from django.contrib import admin
 from buildings.views import get_simple_apartment_info
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^get_simple_apartment_info/([0-9]+)/$', get_simple_apartment_info)
+    url(r'^get_simple_apartment_info/([0-9]+)/$', get_simple_apartment_info),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
